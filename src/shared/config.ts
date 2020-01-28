65b876bb846213351617f25d77440e790eb4a2ce
Max Wu <chengmaxwu@gmail.com>
2020-01-27 18:18:18 -0800

    added shared.ts

---
+++ src/shared/config.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  gold: "#F6D58B"
+};
+
+export const fonts = {
+  title: "Circular Std"
+};
+
+export const mediaQueries = {
+  mobile: "@media (max-width: 700px)",
+  notMobile: "@media (min-width: 701px)"
+};
