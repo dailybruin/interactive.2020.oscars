ab034a34cdc3a1ed8d65415fef1c71cdb544d2cb
Jay Park <jongyunjaypark@gmail.com>
2020-01-27 19:28:06 -0800

    template for the cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Poll from "./components/Poll/Poll";
 import Question from "./components/Question/Question";
 import PollNumber from "./components/PollNumber/PollNumber";
+import Card from "./components/Card/Card";
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Question></Question>
         <Poll></Poll>
         <PollNumber></PollNumber>
+        <Card></Card>
       </header>
     </div>
   );
