cb6179310423db1eb136f1594a4de468c2808990
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-01-29 23:26:19 -0800

    moved

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+const MongoHandler = require("./mongo/mongohandler");
+const mongoHandler = new MongoHandler();
+
+var app = express();
+
+app.post("/", function(req, res) {
+  var answer = req.body.answer;
+  var questionId = req.body.questionId;
+
+  mongoHandler.makeAnswer(answer, questionId);
+});
+
+app.get("/api/questions/:id", function(req, res) {
+  var questionID = req.params.id;
+
+  res.send({ counts: mongoHandler.getCounts(questionID) });
+});
+
+app.listen(3000, function() {
+  console.log("listening on port 3000");
+});
