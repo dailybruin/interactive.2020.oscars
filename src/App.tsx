06b12d94c3327467c9cea0197ef3a6cdf872f91b
Jay Park <jongyunjaypark@gmail.com>
2020-02-04 19:50:51 -0800

    added cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Card from "./components/Card/Card";
 import Poll from "./components/Poll/Poll";
 import Question from "./components/Question/Question";
 import PollNumber from "./components/PollNumber/PollNumber";
 import ChartCard from "./components/ChartCard/ChartCard";
 
-
 var data = {
   datasets: [
     {
-      data: [
-        '1'
-      ],
-      backgroundColor: [
-        "#F6D58B"
-      ],
-      label: 'Dataset 1',
-    },
+      data: ["1"],
+      backgroundColor: ["#F6D58B"],
+      label: "Dataset 1"
+    }
   ],
-  labels: [
-    'hi'
-  ],
-}
-import Card from "./components/Card/Card";
+  labels: ["hi"]
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <ChartCard userData = {data} dbData = {data}/>
+        <ChartCard userData={data} dbData={data} />
         <br></br>
         <Question></Question>
         <Poll></Poll>
