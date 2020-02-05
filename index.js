0ae12ed88f9a7b8412dc772d7984d7b9aaa20092
thenry3 <43651306+thenry3@users.noreply.github.com>
2020-02-04 19:27:07 -0800

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require("express");
+var cors = require("cors");
+
 const bodyParser = require("body-parser");
 const MongoHandler = require("./mongo/mongohandler");
 const mongoHandler = new MongoHandler();
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors);
 
 app.post("/api/sendanswer", function(req, res) {
   var answer = req.body.answer;
